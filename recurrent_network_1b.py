9c526fe94f255e06926dec59aa34b7b7b18cdbaf
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2016-01-05 18:22:18 +0100

    - LSTM continuous regression / float output

--- recurrent_network_1b.py
+++ recurrent_network_1b.py
@@ -8,8 +8,10 @@ import matplotlib.pylab as pl
 
 def gen_data(length=100):
     t = np.linspace(0, length, length, endpoint=False)
-    s = np.sin(t * 0.1)
-    return s
+    s = np.zeros_like(t)
+    for i in range(3):
+        s += np.sin(t * (0.1 + np.random.uniform(-0.01, 0.01)))
+    return s/np.max(np.abs(s))
 
 def get_seq_input_data(ptr, data):
     # seq_input_data = np.random.rand(n_steps, batch_size, seq_width).astype('float32')
@@ -25,15 +27,16 @@ def get_seq_input_data(ptr, data):
 
 if __name__ == '__main__':
     np.random.seed(1)      
-    size = 10
+    size = 100
     batch_size= 1 # 100
-    n_steps = 200
+    n_steps = 500
     seq_width = 1
 
     data_pointer = 0
-    data = gen_data(10000)
+    data = gen_data(1000000)
 
-    initializer = tf.random_uniform_initializer(-0.8,0.8)
+    # initializer = tf.random_uniform_initializer(-0.8,0.8)
+    initializer = tf.random_uniform_initializer(-0.00001,0.00001)
     # initializer = tf.zeros_initializer((size*2,1), dtype=tf.float32)
 
     seq_input = tf.placeholder(tf.float32, [n_steps, batch_size, seq_width])
@@ -44,12 +47,13 @@ if __name__ == '__main__':
     inputs = [tf.reshape(i, (batch_size, seq_width)) for i in tf.split(0, n_steps, seq_input)]
     # inputs for rnn needs to be a list, each item being a timestep. 
     # we need to split our input into each timestep, and reshape it because split keeps dims by default
-    # result = tf.placeholder(tf.float32, [n_steps, batch_size, seq_width])
-    result = tf.placeholder(tf.float32, [None, seq_width])
+    target = tf.placeholder(tf.float32, [n_steps, batch_size, seq_width])
+    # targets = [tf.reshape(i, (batch_size, seq_width)) for i in tf.split(0, n_steps, target)]
+    # target = tf.placeholder(tf.float32, [None, None, seq_width])
 
     cell = LSTMCell(size, seq_width, initializer=initializer)  
-    # initial_state = cell.zero_state(batch_size, tf.float32)
-    initial_state = tf.random_uniform([batch_size, cell.state_size], -0.1, 0.1)
+    initial_state = cell.zero_state(batch_size, tf.float32)
+    # initial_state = tf.random_uniform([batch_size, cell.state_size], -0.1, 0.1)
     outputs, states = rnn.rnn(cell, inputs, initial_state=initial_state, sequence_length=early_stop)
     # set up lstm
     final_state = states[-1]
@@ -60,7 +64,20 @@ if __name__ == '__main__':
     print "type(outputs)", type(outputs)
     output = tf.reshape(tf.concat(1, outputs), [-1, size])
     output = tf.nn.xw_plus_b(output, W_o, b_o)
+    output2 = tf.reshape(output, [n_steps, batch_size, seq_width])
+    print "type(output)", type(output)
     # self.final_state = states[-1]
+
+    # cost = output - target
+    cost = tf.reduce_mean(tf.pow(output2 - target, 2)) # MSE
+    learning_rate = 0.001
+    # print "trainable", tf.trainable_variables()
+    
+    # tvars = tf.trainable_variables()
+    # grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), 10.)
+    # optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    # train_op = optimizer.apply_gradients(zip(grads, tvars))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Adam Optimizer
     
     iop = tf.initialize_all_variables()
     # create initialize op, this needs to be run by the session!
@@ -71,20 +88,60 @@ if __name__ == '__main__':
     # prev_state = session.run(cell.zero_state(batch_size, tf.float32))
     prev_state = session.run(tf.random_uniform([batch_size, cell.state_size], -1., 1.))
 
-    allouts = []
-    for i in range(3):
-        seq_input_data = get_seq_input_data(data_pointer, data)
+    allouts  = []
+    allcosts = []
+    # training
+    for i in range(400):
+        seq_input_data  = get_seq_input_data(data_pointer, data)
+        seq_target_data = get_seq_input_data(data_pointer+1, data)
+
+        feed = {early_stop: n_steps,
+                seq_input: seq_input_data,
+                initial_state: prev_state,
+                target: seq_target_data}
+            
+        # fstate, _ = session.run([final_state, train_op], feed_dict=feed)
+        session.run(optimizer, feed_dict=feed)
+        # prev_state = fstate
+
+        if i % 1 == 0:
+            tcost = session.run(cost, feed_dict=feed)
+            allcosts.append(tcost)
+            # print len(tcost)
+            print "cost[%d] = %f" % (i, tcost)
+        
         data_pointer += n_steps
-        print "pstate", prev_state
-        feed = {early_stop:n_steps, seq_input: seq_input_data, initial_state: prev_state}
+
+    pl.plot(allcosts)
+    pl.show()
+        
+    # eval
+    data_pointer = 0
+    allouts  = []
+    allcosts = []
+    for i in range(3):
+        seq_input_data  = get_seq_input_data(data_pointer, data)
+        seq_target_data = get_seq_input_data(data_pointer+1, data)
+        # print type(seq_target_data)
+        data_pointer += n_steps
+        # print "pstate", prev_state
+        feed = {early_stop: n_steps,
+                seq_input: seq_input_data,
+                initial_state: prev_state,
+                target: seq_target_data}
         # feed = {early_stop:n_steps, seq_input: seq_input_data}
         # define our feeds. 
         # early_stop can be varied, but seq_input needs to match the shape that was defined earlier
 
-        outs, fstate = session.run([output, final_state], feed_dict=feed)
+        outs, fstate, tcost = session.run([output, final_state, cost], feed_dict=feed)
+        # outs, fstate, tcost, opt = session.run([output, final_state, cost, optimizer], feed_dict=feed)
         prev_state = fstate
-        print "fstate", fstate
+        # print "fstate", fstate,tcost
         allouts.append(outs)
+        allcosts.append(tcost)
+        # tcost = session.run(cost, feed_d)
+        if i % 10 == 0:
+            print "cost[%d] = %f" % (i, tcost)
     
     # run once
     # output is a list, each item being a single timestep. Items at t>early_stop are all 0s
@@ -95,15 +152,20 @@ if __name__ == '__main__':
     print outs[0].shape
     print "allouts", len(allouts)
 
-    pl.subplot(211)
+    pl.subplot(311)
     pl.plot(seq_input_data[:,0,:])
-    pl.subplot(212)
+    pl.plot(seq_target_data[:,0,:])
+    pl.plot(outs)
+    pl.subplot(312)
     
     # for i,out in enumerate(allouts):
     #     print out.shape
     #     pl.plot(out)
     allouts = np.asarray(allouts)
     pl.plot(allouts.flatten())
+
+    pl.subplot(313)
+    pl.plot(allcosts)
     
     # pl.plot(outs)
     pl.show()
