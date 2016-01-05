4dc005db270e10c848f880625c308789e002f03b
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2016-01-05 15:04:06 +0100

    - 1b concat output, retaining state!

--- recurrent_network_1b.py
+++ recurrent_network_1b.py
@@ -6,19 +6,32 @@ from tensorflow.models.rnn.rnn_cell import BasicLSTMCell, LSTMCell
 import numpy as np
 import matplotlib.pylab as pl
 
-def get_seq_input_data():
+def gen_data(length=100):
+    t = np.linspace(0, length, length, endpoint=False)
+    s = np.sin(t * 0.1)
+    return s
+
+def get_seq_input_data(ptr, data):
     # seq_input_data = np.random.rand(n_steps, batch_size, seq_width).astype('float32')
     seq_input_data = np.zeros((n_steps, batch_size, seq_width)).astype('float32')
-    seq_input_data[0, :, :] = 1.
-    seq_input_data[n_steps/2, :, :] = -1.
+    # seq_input_data[0, :, :] = 1.
+    # seq_input_data[n_steps/2, :, :] = -1.
+    for n in xrange(batch_size):
+        # print n
+        seq_input_data[:,n,:] = data[ptr:ptr+n_steps,np.newaxis]
+        # pass
+    
     return seq_input_data
 
 if __name__ == '__main__':
     np.random.seed(1)      
     size = 10
-    batch_size= 3 # 100
+    batch_size= 1 # 100
     n_steps = 200
     seq_width = 1
+
+    data_pointer = 0
+    data = gen_data(10000)
 
     initializer = tf.random_uniform_initializer(-0.8,0.8)
     # initializer = tf.zeros_initializer((size*2,1), dtype=tf.float32)
@@ -33,7 +46,6 @@ if __name__ == '__main__':
     # we need to split our input into each timestep, and reshape it because split keeps dims by default
     # result = tf.placeholder(tf.float32, [n_steps, batch_size, seq_width])
     result = tf.placeholder(tf.float32, [None, seq_width])
-    
 
     cell = LSTMCell(size, seq_width, initializer=initializer)  
     # initial_state = cell.zero_state(batch_size, tf.float32)
@@ -56,13 +68,13 @@ if __name__ == '__main__':
     session.run(iop)
     # actually initialize, if you don't do this you get errors about uninitialized stuff
 
-    seq_input_data = get_seq_input_data()
-    
     # prev_state = session.run(cell.zero_state(batch_size, tf.float32))
     prev_state = session.run(tf.random_uniform([batch_size, cell.state_size], -1., 1.))
 
     allouts = []
     for i in range(3):
+        seq_input_data = get_seq_input_data(data_pointer, data)
+        data_pointer += n_steps
         print "pstate", prev_state
         feed = {early_stop:n_steps, seq_input: seq_input_data, initial_state: prev_state}
         # feed = {early_stop:n_steps, seq_input: seq_input_data}
@@ -86,8 +98,12 @@ if __name__ == '__main__':
     pl.subplot(211)
     pl.plot(seq_input_data[:,0,:])
     pl.subplot(212)
-    for i,out in enumerate(allouts):
-        print out.shape
-        pl.plot(out)
+    
+    # for i,out in enumerate(allouts):
+    #     print out.shape
+    #     pl.plot(out)
+    allouts = np.asarray(allouts)
+    pl.plot(allouts.flatten())
+    
     # pl.plot(outs)
     pl.show()
